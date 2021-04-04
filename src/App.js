475cecf13bfebfafeecc6a19435c31cba873bd42
Adam Devigili <adam.devigili@gmail.com>
2021-04-04 16:24:56 -0500

    add sample 3D scatter plot

--- src/App.js
+++ src/App.js
@@ -1,56 +1,49 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import './App.css';
+import Plot from 'react-plotly.js';
+
+function randomValues(num, mul) {
+  const arr = [];
+  const index = [];
+  for (let i = 0; i < num; i++) {
+    arr.push(Math.random() * mul)
+    index.push(i);
+  }
+  return {index, arr};
+}
 
 function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
+  // const [date, setDate] = useState(null);
+  // useEffect(() => {
+  //   async function getDate() {
+  //     const res = await fetch('/api/date');
+  //     const newDate = await res.text();
+  //     setDate(newDate);
+  //   }
+  //   getDate();
+  // }, []);
+
+  const traces = Array(3).fill(0).map((_, i) => {
+    const {index, arr} = randomValues(20, 3);
+    return {
+      x: Array(20).fill(i),
+      y: index,
+      z: arr,
+      type: 'scatter3d',
+      mode: 'lines'
     }
-    getDate();
-  }, []);
+  });
+
   return (
-    <main>
-      <h1>Create React App + Go API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/master/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
-    </main>
+    <Plot
+      data={traces}
+      layout={{
+        width: 900,
+        height: 800,
+        title: `Simple 3D Scatter`
+      }}
+    />
   );
 }
 
