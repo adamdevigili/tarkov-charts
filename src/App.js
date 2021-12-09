9e03b3bd43f5d87e9f66ed697e1b659042dffc24
Adam Devigili <adam.devigili@gmail.com>
2021-12-08 23:48:44 -0600

    huge UI update, slowly looking like an actual app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import './App.css';
+import "./App.css";
 
-import Plot from 'react-plotly.js';
-import createTracesFromJSON from './Traces.js'
+import Plot from "react-plotly.js";
+import createTracesFromJSON from "./Traces.js";
 
-import Spinner from 'react-bootstrap/Spinner'
-import Container from 'react-bootstrap/Container'
-import Col from 'react-bootstrap/Col'
-import Row from 'react-bootstrap/Row'
+import Spinner from "react-bootstrap/Spinner";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
-import TCNavbar from './components/Navbar';
+import TCNavbar from "./components/Navbar";
 
 function App() {
   const [appState, setAppState] = useState({
@@ -20,17 +20,17 @@ function App() {
 
   useEffect(() => {
     setAppState({ loading: true });
-    let endpoint = ""
+    let endpoint = "";
     if (process.env.REACT_APP_API_ENDPOINT) {
-      endpoint = process.env.REACT_APP_API_ENDPOINT
+      endpoint = process.env.REACT_APP_API_ENDPOINT;
     } else {
-      endpoint = "https://www.tarkov-charts.com"
+      endpoint = "https://www.tarkov-charts.com";
     }
-    const apiUrl = endpoint + "/api/ammo"
+    const apiUrl = endpoint + "/api/ammo";
     fetch(apiUrl, {
       headers: {
-        'X-Tarkov-Charts-API-Key': process.env.REACT_APP_TC_API_KEY
-      }
+        "X-Tarkov-Charts-API-Key": process.env.REACT_APP_TC_API_KEY,
+      },
     })
       .then((res) => res.json())
       .then((ammoData) => {
@@ -38,86 +38,130 @@ function App() {
       });
   }, [setAppState]);
 
-  let finalTraces
-  let updatedAt
-  let releaseVersion = process.env.REACT_APP_RELEASE_VERSION
+  let finalTraces;
+  let updatedAt;
+  let releaseVersion = process.env.REACT_APP_RELEASE_VERSION;
   if (appState.ammoData) {
-    finalTraces = createTracesFromJSON(appState.ammoData)
-    updatedAt = appState.ammoData["_updated_at"]
+    finalTraces = createTracesFromJSON(appState.ammoData);
+    updatedAt = appState.ammoData["_updated_at"];
   }
 
-  const plot = <Plot
-    config={{displayModeBar: false}}
-    data={finalTraces}
-    layout={{
-      paper_bgcolor:"rgb(230,230,230)",
-      height: 1000,
-      width: 1200,
-      title: `Tarkov Ammo by Caliber: Damage/Penetration/Price (last updated ` + updatedAt + `)`,
-      autosize: true,
-      scene: {
-        xaxis: {
-          title: "Damage",
-          range: [200, 0],
+  const plot = (
+    <Plot
+      config={{ displayModeBar: false }}
+      data={finalTraces}
+      layout={{
+        // paper_bgcolor: "rgb(230,230,230)",
+        paper_bgcolor: "rgb(38,38,38)",
+        plot_bgcolor: "rgb(0,0,255)",
+        height: 1000,
+        width: 1200,
+        title:
+          `Tarkov Ammo by Caliber: Damage/Penetration/Price (last updated ` +
+          updatedAt +
+          `)`,
+        font: {
+          family: "Bender",
+          size: 14,
+          color: "rgb(255,255,255)",
         },
-        yaxis: {
-          title: "Penetration",
-          range: [80, 0],
+        autosize: true,
+        scene: {
+          xaxis: {
+            color: "rgb(255,255,255)",
+            title: "Damage",
+            range: [200, 0],
+          },
+          yaxis: {
+            color: "rgb(255,255,255)",
+            title: "Penetration",
+            range: [80, 0],
+          },
+          zaxis: {
+            color: "rgb(255,255,255)",
+            title: "Cost (₽, 24hr avg.)",
+            range: [0, 5000],
+          },
+          camera: {
+            center: { x: 0.05, y: 0.075, z: -0.15 },
+            eye: { x: 1.35, y: 1.4, z: 1 },
+          },
         },
-        zaxis: {
-          title: "Cost (₽, 24hr avg.)",
-          range: [0, 5000],
-        },
-        camera: {
-          center: {x: 0.05, y: 0.075, z: -0.15},
-          eye: {x: 1.35, y: 1.4, z: 1}
-        }
-      }
-    }}
-    useResizeHandler={true}
-    style={{width: "100%", height: "100%"}}
-    // onUpdate={() => console.log(plot)}
-  />
+      }}
+      useResizeHandler={true}
+      style={{ width: "100%", height: "100%" }}
+      // onUpdate={() => console.log(plot)}
+    />
+  );
 
   return (
-    <div style={{backgroundColor: '#262626', height: '100vh'}}>
+    <div style={{ backgroundColor: "#262626", height: "100vh" }}>
       <div>
-        <TCNavbar/>
+        <TCNavbar />
       </div>
       <div>
         {appState.loading ? (
-          <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100vh",
+            }}
+          >
             <div>
-              <Spinner animation="border" role="status" style={{color: '#e6e6e6'}}/>
+              <Spinner
+                animation="border"
+                role="status"
+                style={{ color: "#e6e6e6" }}
+              />
             </div>
           </div>
-          ):(
-          <Container fluid className='gx-0'>
-            <Row className='gx-0'>
-              <Col style={{color: '#e6e6e6'}}>
+        ) : (
+          <Container fluid className="gx-0">
+            <Row className="gx-0">
+              <Col style={{ color: "#e6e6e6" }}>
                 <ul>
-                  <li><b>Single-click</b> a caliber to remove/add it to the graph</li>
-                  <li><b>Double-click</b> a caliber to isolate it</li>
-                  <li><b>Left click+drag</b> to rotate</li>
-                  <li><b>Right click+drag</b> to pan</li>
-                  <li><b>Mouse wheel</b> to zoom</li>
-                  <li><b>Ctrl+click</b> to add single calibers to the graph</li>
+                  <li>
+                    <b>Single-click</b> a caliber to remove/add it to the graph
+                  </li>
+                  <li>
+                    <b>Double-click</b> a caliber to isolate it
+                  </li>
+                  <li>
+                    <b>Left click+drag</b> to rotate
+                  </li>
+                  <li>
+                    <b>Right click+drag</b> to pan
+                  </li>
+                  <li>
+                    <b>Mouse wheel</b> to zoom
+                  </li>
+                  <li>
+                    <b>Ctrl+click</b> to add single calibers to the graph
+                  </li>
                 </ul>
               </Col>
-              <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto" xxl="auto"> 
-                <div className='main-plot'> </div>
+              <Col xs="auto" sm="auto" md="auto" lg="auto" xl="auto" xxl="auto">
+                <div className="main-plot"> </div>
                 <div>{plot}</div>
               </Col>
-              <Col className='justify-content-end'>
+              <Col className="justify-content-end">
                 <div className="release-version">
-                <a href={"https://github.com/adamdevigili/tarkov-charts/releases/tag/" + releaseVersion} rel="noreferrer">
-                  {releaseVersion}
-                </a>
+                  <a
+                    href={
+                      "https://github.com/adamdevigili/tarkov-charts/releases/tag/" +
+                      releaseVersion
+                    }
+                    rel="noreferrer"
+                  >
+                    {releaseVersion}
+                  </a>
                 </div>
               </Col>
             </Row>
           </Container>
-          )}
+        )}
       </div>
     </div>
   );
--- src/Traces.js
+++ src/Traces.js
@@ -1,90 +1,90 @@
 function createTracesFromJSON(ammoData) {
-    let traces = []
+  let traces = [];
 
+  const baseTrace = {
+    type: "scatter3d",
+    mode: "lines+markers+text",
+    textposition: "top center",
+    marker: { size: 3 },
+    hovertemplate:
+      "<extra></extra>" +
+      "<b><i>%{text}</i></b><br>" +
+      "Damage: %{x}<br>" +
+      "Pen: %{y}<br>" +
+      "Cost: ₽ %{z}<br>",
+  };
 
-    const baseTrace = {
-        type: 'scatter3d',
-        mode: 'lines+markers+text',
-        textposition: 'top center',
-        marker: {size: 3},
-        hovertemplate:
-            '<extra></extra>' +
-            '<b><i>%{text}</i></b><br>' +
-            'Damage: %{x}<br>' +
-            'Pen: %{y}<br>' +
-            'Cost: ₽ %{z}<br>'
+  ammoData = ammoData["data"];
+
+  for (const caliber in ammoData) {
+    let trace = {
+      ...baseTrace,
+      name: caliber,
+      x: [],
+      y: [],
+      z: [],
+      text: [],
+    };
+
+    let ammoArray = [];
+
+    for (const ammoID in ammoData[caliber]) {
+      for (const _ in ammoData[caliber][ammoID]) {
+        ammoArray.push(ammoData[caliber][ammoID]);
+      }
     }
 
-    ammoData = ammoData["data"]
+    let ammoName = ammoArray[0].name;
+    trace.name = ammoName.substr(0, ammoName.indexOf(" ")).replace("mm", "");
+    ammoArray.sort((a, b) => {
+      if (a.penetration > b.penetration) return 1;
+      if (a.penetration < b.penetration) return -1;
+      return 0;
+    });
 
-    for (const caliber in ammoData) {
-        let trace = {
-            ...baseTrace,
-            name: caliber,
-            x:[],
-            y:[],
-            z:[],
-            text: []
-        }
+    for (const ammo of ammoArray) {
+      if (ammo.damage > 200) {
+        trace.x.push(200);
+      } else {
+        trace.x.push(ammo.damage);
+      }
 
-        let ammoArray = []
+      trace.y.push(ammo.penetration);
 
-        for (const ammoID in ammoData[caliber]) {
-            for (const _ in ammoData[caliber][ammoID]) {
-                ammoArray.push(ammoData[caliber][ammoID])
-            }
-        }
-
-        let ammoName = ammoArray[0].name
-        trace.name = ammoName.substr(0, ammoName.indexOf(' ')).replace("mm", "");
-        ammoArray.sort((a,b) => {
-            if (a.penetration > b.penetration) return 1
-            if (a.penetration < b.penetration) return -1
-            return 0
-        })
-
-        for (const ammo of ammoArray) {
-            if (ammo.damage > 200) {
-                trace.x.push(200)
-            } else {
-                trace.x.push(ammo.damage)
-            }
-
-            trace.y.push(ammo.penetration)
-
-            if (ammo.price > 5000) {
-                trace.z.push(5000)
-            } else {
-                trace.z.push(ammo.price)
-            }
-            trace.text.push(ammo.shortname)
-        }
-
-        if (caliber === "Caliber12g" || 
-            caliber === "Caliber9x18PM" ||
-            caliber === "Caliber762x25TT" ||
-            caliber === "Caliber9x21" ||
-            caliber === "Caliber30x29" ||
-            caliber === "Caliber366TKM" ||
-            caliber === "Caliber762x35" ||
-            caliber === "Caliber1143x23ACP" ||
-            caliber === "Caliber127x108" ||
-            caliber === "Caliber23x75" ||
-            caliber === "Caliber127x55" ||
-            caliber === "Caliber545x39" ||
-            caliber === "Caliber57x28" ||
-            caliber === "Caliber762x54R" ||
-            caliber === "Caliber86x70" ||
-            caliber === "Caliber40x46" ||
-            caliber === "Caliber20g"
-        ) {
-            trace.visible = 'legendonly'
-        }
-        
-        traces.push(trace)
+      if (ammo.price > 5000) {
+        trace.z.push(5000);
+      } else {
+        trace.z.push(ammo.price);
+      }
+      trace.text.push(ammo.shortname);
     }
 
-    return traces
+    if (
+      caliber === "Caliber12g" ||
+      caliber === "Caliber9x18PM" ||
+      caliber === "Caliber762x25TT" ||
+      caliber === "Caliber9x21" ||
+      caliber === "Caliber30x29" ||
+      caliber === "Caliber366TKM" ||
+      caliber === "Caliber762x35" ||
+      caliber === "Caliber1143x23ACP" ||
+      caliber === "Caliber127x108" ||
+      caliber === "Caliber23x75" ||
+      caliber === "Caliber127x55" ||
+      caliber === "Caliber545x39" ||
+      caliber === "Caliber57x28" ||
+      caliber === "Caliber762x54R" ||
+      caliber === "Caliber86x70" ||
+      caliber === "Caliber40x46" ||
+      caliber === "Caliber20g"
+    ) {
+      trace.visible = "legendonly";
+    }
+
+    traces.push(trace);
+  }
+
+  return traces;
 }
 
-export default createTracesFromJSON
+export default createTracesFromJSON;
